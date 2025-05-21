84545750ef427f6f9083fffe56ca0fe1f387d3cf
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-21 15:54:18 +0200

    Back to dev

--- classes/Dic.php
+++ classes/Dic.php
@@ -28,7 +28,7 @@ use Plib\View;
 
 class Dic
 {
-    public const VERSION = "1.0";
+    public const VERSION = "1.1-dev";
 
     public static function mainCommand(): MainCommand
     {
--- tests/InfoCommandTest.php
+++ tests/InfoCommandTest.php
@@ -32,7 +32,7 @@ class InfoCommandTest extends TestCase
     public function testRendersPluginInfo(): void
     {
         $response = $this->sut()();
-        $this->assertSame("Maps 1.0", $response->title());
+        $this->assertSame("Maps 1.1-dev", $response->title());
         Approvals::verifyHtml($response->output());
     }
 }
